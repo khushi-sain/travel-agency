ce0f6e9196d6290a6e55940bf7ba00668da5d138
khushi-sain <149334164+khushi-sain@users.noreply.github.com>
2025-04-10 12:52:05 +0530

    Delete src directory

--- src/config/dotenv.js
+++
@@ -1,7 +0,0 @@
-const dotenv = require('dotenv'); 
-dotenv.config(); 
-
-module.exports = {
-    PORT: process.env.PORT || 5000, 
-    DB_URI: process.env.DB_URI || 'mongodb://localhost:27017/travelDB'
-};
--- src/server.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config(); // Load environment variables
-const PORT = process.env.PORT || 5000;
-const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/travelDB';
